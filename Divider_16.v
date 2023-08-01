e0fddd47459ffecbcf164ae5d45dac81f5a6593c
TargetToVLSI <138660909+TargetToVLSI@users.noreply.github.com>
2023-08-01 14:59:37 +0530

    Add files via upload

---
+++ Divider_16.v
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    14:33:08 08/01/2023 
+// Design Name: 
+// Module Name:    Divider_16 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Divider_16( input[3:0]dividend,divisor,
+    output reg [3:0]quotient,remainder);
+
+    always@(dividend,divisor)
+        begin
+        quotient = 0;
+        remainder= dividend;
+            while(remainder>=divisor)
+            begin	
+                remainder = remainder - divisor;
+                quotient = quotient + 1;
+            end
+        end
+endmodule
+
+
